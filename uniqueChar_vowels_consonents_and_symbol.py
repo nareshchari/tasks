c1c723f85195c9c7e2fe8c57b5e781e043407978
nareshchari <36660774+nareshchari@users.noreply.github.com>
2020-12-07 22:30:13 +0530

    Create uniqueChar_vowels_consonents_and_symbol.py

---
+++ uniqueChar_vowels_consonents_and_symbol.py
@@ -0,0 +1,34 @@
+vowels = "aeiou"
+# no need for cpital alpha because we are making it into lower case
+alnum_ = "abcdefghijklmnopqrstuvwxyz0123456789"
+temp = 0
+while True:
+    # taking input from user and modifying into lowercase to avoid case sensitive and removing spaces before and after
+    statement = input("\nEnter your input: ").lower().strip()
+    # to avoid single spce
+    statement = statement.replace(' ','')
+    if statement != "":
+        for each_char in statement:
+            k = statement.find(each_char,statement.index(each_char)+1,len(statement))
+            if k == -1:
+                temp += 1
+                if each_char in vowels:
+                    print(f"\nunique character is {each_char} and is vowel\n")
+                elif each_char in "0123456789":
+                    # not considering numbers
+                    pass
+                # checking symbol or not
+                elif each_char not in alnum_:
+                    print(f"\n{each_char}  is symbol and  {each_char.encode()} encoded value  of symbols\n")
+                else:
+                    print(f"\nunique character is {each_char} and is consonent\n")
+        if temp == 0:
+            print("\nThere is no unique letter\n")
+        # taking user decission
+        user_input = input("\nwould you like to continue y/n..? :").lower()
+        if user_input != 'y':
+            break
+        else:
+            pass
+    else:
+        print("\nplease enter valid input\n")
