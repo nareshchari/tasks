e6ccb2e82f60876b85565b530061e6316d56e6dc
nareshchari <36660774+nareshchari@users.noreply.github.com>
2020-12-10 09:02:48 +0530

    Update genrandom.py

--- genrandom.py
+++ genrandom.py
@@ -1,9 +1,12 @@
 import random
+# to get different lists  doesnt contain same values in between lists
 startvalue=1
 endvalue=10
 for genlist_ in range(10):
+    # getting each time fresh list
     randomlist_=[]
     for genval in range(10):
+        # generating random values and appending in list
         randomlist_.append(random.randint(startvalue,endvalue))
     startvalue+=10
     endvalue+=10
